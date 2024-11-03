b97a39cc4d3426c57ccc7bc877e5e8c9cb3d9182
ShiWenber <1210169842@qq.com>
2024-11-03 14:46:51 +0800

    chore: Best archive f1 66

---
+++ best_archive/f1_66/code/functions.py
@@ -0,0 +1,435 @@
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import pandas as pd
+from tqdm import tqdm
+import time
+from utils.augmentation import Augmentation
+import warnings
+
+def augment_data(train_data, train_labels, test_labels):
+    """
+    对每个 msisdn 进行数据增强，并将增强后的数据合并到原始数据中。
+
+    Args:
+        train_data (pd.DataFrame): 训练数据。
+        train_labels (pd.DataFrame): 训练标签。
+        test_labels (pd.DataFrame): 测试标签。
+
+    Returns:
+        pd.DataFrame: 增强后的训练数据。
+        pd.DataFrame: 增强后的训练标签。
+        pd.DataFrame: 增强后的标签。
+    """
+    warnings.warn("This function will be deprecated in the future, use augment_data_parallel instead.", DeprecationWarning)
+
+    addition_train_data = []
+    addition_train_labels = []
+
+    times=2
+    ratio_range=0.1
+    pbar = tqdm(train_data.groupby('msisdn'))
+    for msisdn, group in pbar:
+        if msisdn == 0:
+            continue
+        pbar.set_description(f"Augmenting msisdn {msisdn}")
+        label = train_labels[train_labels['msisdn'] == msisdn].iloc[0]['is_sa']
+        aug = Augmentation(group, label, 'msisdn', 'is_sa')
+        if label == 1:
+            res_df, res_labels = aug.times(ratio=ratio_range, times=3+4*times, method='mask')
+            addition_train_data.append(res_df)
+            addition_train_labels.append(res_labels)
+        else:
+            res_df, res_labels = aug.times(ratio=ratio_range, times=times, method='mask')
+            addition_train_data.append(res_df)
+            addition_train_labels.append(res_labels)
+
+    addition_train_data = pd.concat(addition_train_data)
+    addition_train_labels = pd.concat(addition_train_labels)
+
+    # 将新数据加入到train_data中
+    train_data = pd.concat([train_data, addition_train_data], ignore_index=True).reset_index(drop=True)
+    train_labels = pd.concat([train_labels, addition_train_labels], ignore_index=True).reset_index(drop=True)
+
+    # 按照 msisdn, start_time 排序
+    sort_start_time = time.time()
+    train_data = train_data.sort_values(by=['msisdn', 'start_time']).reset_index(drop=True)
+    train_labels = train_labels.sort_values(by=['msisdn']).reset_index(drop=True)
+    print('sort time:', time.time() - sort_start_time)
+
+    labels_aug = pd.concat([train_labels, test_labels], ignore_index=True).reindex()
+
+    return train_data, train_labels, labels_aug
+
+def _augment_group(group_data, train_labels):
+    addition_train_data = []
+    addition_train_labels = []
+    times = 2
+    ratio_range = 0.1
+    for msisdn, group in group_data.groupby('msisdn'):
+        if msisdn == 0:
+            continue
+        label = train_labels[train_labels['msisdn'] == msisdn].iloc[0]['is_sa']
+        aug = Augmentation(group, label, 'msisdn', 'is_sa')
+        if label == 1:
+            res_df, res_labels = aug.times(ratio=ratio_range, times=3+4*times, method='mask')
+            addition_train_data.append(res_df)
+            addition_train_labels.append(res_labels)
+        else:
+            res_df, res_labels = aug.times(ratio=ratio_range, times=times, method='mask')
+            addition_train_data.append(res_df)
+            addition_train_labels.append(res_labels)
+    return pd.concat(addition_train_data), pd.concat(addition_train_labels)
+
+def augment_data_parallel(train_data, train_labels, test_labels, max_workers=0):
+    if max_workers == 0:
+        max_workers = os.cpu_count()
+
+    # Split the data into max_workers groups
+    groups = [group for _, group in train_data.groupby('msisdn')]
+    print("len(groups):", len(groups))
+    chunk_size = len(groups) // max_workers
+    chunks = [pd.concat(groups[i - chunk_size:i]) for i in range(chunk_size, len(groups), chunk_size)]
+    # BUG: 获取 len(groups) % max_workers 个数据合入到最后一个 chunk 中
+    if len(groups) % max_workers != 0:
+        rest_df = pd.concat(groups[-(len(groups) % max_workers):])
+        chunks[-1] = pd.concat([chunks[-1], rest_df])
+
+    addition_train_data = []
+    addition_train_labels = []
+
+    print("parallel augmenting data...")
+    start_time = time.time()
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(_augment_group, chunk, train_labels) for chunk in chunks]
+        print("collecting results...")
+        for future in as_completed(futures):
+            res_data, res_labels = future.result()
+            addition_train_data.append(res_data)
+            addition_train_labels.append(res_labels)
+    print("augmenting done, time:", time.time() - start_time)
+
+    addition_train_data = pd.concat(addition_train_data)
+    addition_train_labels = pd.concat(addition_train_labels)
+
+    # 将新数据加入到train_data中
+    train_data = pd.concat([train_data, addition_train_data], ignore_index=True).reset_index(drop=True)
+    train_labels = pd.concat([train_labels, addition_train_labels], ignore_index=True).reset_index(drop=True)
+
+    # 按照 msisdn, start_time 排序
+    sort_start_time = time.time()
+    train_data = train_data.sort_values(by=['msisdn', 'start_time']).reset_index(drop=True)
+    train_labels = train_labels.sort_values(by=['msisdn']).reset_index(drop=True)
+    print('sort time:', time.time() - sort_start_time)
+
+    labels_aug = pd.concat([train_labels, test_labels], ignore_index=True).reindex()
+
+    return train_data, train_labels, labels_aug
+
+
+def get_nan(train):
+    # 获取 train 中的 nan值
+    train_nan = train[train.isnull().T.any()]
+    # 统计 每列含有的 nan 数量
+    for col in train.columns:
+        if train[col].isnull().sum() > 0:
+            print(col, train[col].isnull().sum())
+
+    return train_nan
+
+# 聚合特征
+def aggregate_features(data):
+    warnings.warn("This function will be deprecated in the future, use aggregate_features_parallel instead.", DeprecationWarning)
+    agg_features = data.groupby('msisdn').agg({
+    'call_duration': [
+        ('sum', 'sum'), 
+        ('mean', 'mean'), 
+        ('max', 'max'), 
+        ('std', 'std'),
+        ('quantile_25', lambda x: x.quantile(0.25)), 
+        ('quantile_50', lambda x: x.quantile(0.50)), 
+        ('quantile_75', lambda x: x.quantile(0.75)),
+    ],
+    'cfee': [
+        ('sum', 'sum'),
+        ('std', 'std'), 
+        ('mean', 'mean'),
+    ],
+    'lfee': [
+        ('sum', 'sum'), 
+        ('mean', 'mean'),
+        ('std', 'std'),
+    ],
+    'hour': [
+        ('mean', 'mean'), 
+        ('std', 'std'), 
+        ('max', 'max'), 
+        ('min', 'min'),
+    ],
+    'dayofweek': [
+        ('std', 'std'), 
+        ('magic', lambda x: x.value_counts().mean()), 
+        ('work_day_num', lambda x: x[x.isin([1,2,3,4,5])].count()), 
+        ('weekend_num', lambda x: x[x.isin([6,7])].count()),
+        ('mode', lambda x: x.mode().values[0]),
+        ('work_day_weekend_diff', lambda x: (x[x.isin([1,2,3,4,5])].count() - x[x.isin([6,7])].count()) / (x[x.isin([1,2,3,4,5])].count() + x[x.isin([6,7])].count())),
+    ],
+    # 'home_area_code': [
+    #     ('home_area_code_nunique', 'nunique')
+    # ],
+    'visit_area_code': [
+        ('nunique', 'nunique'),
+        ('times_not_at_home_area', lambda x: x[x != x.shift()].count()/x.count())
+    ],
+    'called_home_code': [
+        ('nunique', 'nunique'),
+        ('called_diff_home_code', lambda x: x[x != x.shift()].count() / x.count())
+    ],
+    'called_code': [
+        ('nunique', 'nunique'),
+        ('diff', lambda x: x[x != x.shift()].count()/ x.count())
+    ],
+    'open_datetime': [
+        ('open_count', 'nunique')
+    ],
+    'other_party': [
+        ('account_person_num', 'nunique'),
+        ('called_diff_home_code', lambda x: x[x != x.shift()].count() / x.count())
+    ],
+    'a_serv_type': [
+        ('call_num', lambda x: x[x.isin([1, 3])].count()), 
+        ('called_num', lambda x: x[x == 2].count()),
+        ('call_called_normalized_diff', lambda x: (x[x.isin([1, 3])].count() - x[x == 2].count()) /  (x[x.isin([1, 3])].count() + x[x == 2].count())),
+    ],
+    'start_time_diff': [
+        ('start_time_diff_mean', 'mean'), 
+        ('start_time_diff_std', 'std'), 
+        ('max', 'max'), 
+        ('coefficient_of_variation', lambda x: x.std() / x.mean()),
+    ], 
+    # 'phone1_type': [
+    #     ('nunique', 'nunique'),
+    #     ('mode', lambda x: x.mode().values[0])
+    # ],
+    # 'distance': [
+    #     ('sum', 'sum'), 
+    #     ('std', 'std'), 
+    #     ('max', 'max'), 
+    #     ('quantile_25', lambda x: x.quantile(0.25)), 
+    #     ('quantile_50', lambda x: x.quantile(0.50)), 
+    #     ('quantile_75', lambda x: x.quantile(0.75)),
+    # ]
+    })
+
+    agg_features.columns = ['+'.join(col).strip() for col in agg_features.columns.values]
+    agg_features.columns = agg_features.columns.str.replace('[', '').str.replace(']', '').str.replace('<', '').str.replace('>', '').str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace(' ', '_')
+    agg_features = agg_features.reset_index()
+    return agg_features
+
+class _CustomAggregator:
+    @staticmethod
+    def quantile_25(x):
+        return x.quantile(0.25)
+
+    @staticmethod
+    def quantile_50(x):
+        return x.quantile(0.5)
+
+    @staticmethod
+    def quantile_75(x):
+        return x.quantile(0.75)
+
+    # just for column 'dayofweek'
+    @staticmethod
+    def magic(x):
+        return x.value_counts().mean()
+
+    # just for column 'dayofweek'
+    @staticmethod
+    def work_day_num(x):
+        return x[x.isin([1, 2, 3, 4, 5])].count()
+
+    @staticmethod
+    def weekend_num(x):
+        return x[x.isin([6, 7])].count()
+
+    @staticmethod
+    def mode(x):
+        return x.mode().values[0]
+
+    @staticmethod
+    def work_day_weekend_diff(x):
+        work_days = x[x.isin([1, 2, 3, 4, 5])].count()
+        weekends = x[x.isin([6, 7])].count()
+        return (work_days - weekends) / (work_days + weekends)
+
+    @staticmethod
+    def times_not_at_home_area(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def called_diff_home_code(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def diff(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def call_num(x):
+        return x[x.isin([1, 3])].count()
+
+    @staticmethod
+    def called_num(x):
+        return x[x == 2].count()
+
+    @staticmethod
+    def call_called_normalized_diff(x):
+        calls = x[x.isin([1, 3])].count()
+        called = x[x == 2].count()
+        return (calls - called) / (calls + called)
+
+    @staticmethod
+    def coefficient_of_variation(x):
+        return x.std() / x.mean()
+
+def _aggregate_features(data, agg_params):
+    agg_features = data.groupby('msisdn').agg(agg_params)
+    agg_features.columns = ['+'.join(col).strip() for col in agg_features.columns.values]
+    agg_features.columns = agg_features.columns.str.replace('[', '').str.replace(']', '').str.replace('<', '').str.replace('>', '').str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace(' ', '_')
+    agg_features = agg_features.reset_index()
+    return agg_features
+
+# 并行处理函数
+def aggregate_features_parallel(data, max_workers=0):
+    agg_params = {
+        'call_duration': [
+            ('sum', 'sum'), 
+            ('mean', 'mean'), 
+            ('max', 'max'), 
+            ('std', 'std'),
+            ('quantile_25', _CustomAggregator.quantile_25),
+            ('quantile_50', _CustomAggregator.quantile_50),
+            ('quantile_75', _CustomAggregator.quantile_75),
+        ],
+        'cfee': [
+            ('sum', 'sum'),
+            ('std', 'std'), 
+            ('mean', 'mean'),
+        ],
+        'lfee': [
+            ('sum', 'sum'), 
+            ('mean', 'mean'),
+            ('std', 'std'),
+        ],
+        'hour': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'dayofweek': [
+            ('std', 'std'), 
+            ('magic', _CustomAggregator.magic),
+            ('work_day_num', _CustomAggregator.work_day_num),
+            ('weekend_num', _CustomAggregator.weekend_num),
+            ('mode', _CustomAggregator.mode),
+            ('work_day_weekend_diff', _CustomAggregator.work_day_weekend_diff),
+        ],
+        'visit_area_code': [
+            ('nunique', 'nunique'),
+            ('times_not_at_home_area', _CustomAggregator.times_not_at_home_area)
+        ],
+        'called_home_code': [
+            ('nunique', 'nunique'),
+            ('called_diff_home_code', _CustomAggregator.called_diff_home_code)
+        ],
+        'called_code': [
+            ('nunique', 'nunique'),
+            ('diff', _CustomAggregator.diff)
+        ],
+        'open_datetime': [
+            ('open_count', 'nunique')
+        ],
+        'other_party': [
+            ('account_person_num', 'nunique'),
+            ('called_diff_home_code', _CustomAggregator.called_diff_home_code)
+        ],
+        'a_serv_type': [
+            ('call_num', _CustomAggregator.call_num), 
+            ('called_num', _CustomAggregator.called_num),
+            ('call_called_normalized_diff', _CustomAggregator.call_called_normalized_diff),
+        ],
+        'start_time_diff': [
+            ('start_time_diff_mean', 'mean'), 
+            ('start_time_diff_std', 'std'), 
+            ('max', 'max'), 
+            ('coefficient_of_variation', _CustomAggregator.coefficient_of_variation),
+        ], 
+        'phone1_loc_city_lat': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone1_loc_city_lon': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone2_loc_city_lat': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone2_loc_city_lon': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'start_day': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'start_day_diff': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+    }
+
+    if max_workers == 0:
+        max_workers = min(os.cpu_count(), len(agg_params))
+    print(f'Using {max_workers} workers to aggregate features...')
+
+    columns = [col for col in data.columns if col in agg_params.keys()]
+    results = []
+
+    unused_params = [i for i in agg_params.keys() if i not in columns]
+    if unused_params:
+        print(f"The following aggregate params are not found in the dataframe and will be ignored:\n{', '.join(unused_params)}")
+
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(_aggregate_features, data[['msisdn', col]], agg_params[col]): col for col in columns}
+        for future in as_completed(futures):
+            col = futures[future]
+            try:
+                result = future.result()
+                results.append(result)
+            except Exception as exc:
+                print(f'{col} generated an exception: {exc}')
+
+    # # 验证每个结果的 msisdn 是否一致
+    # for i in range(1, len(results)):
+    #     pd.testing.assert_series_equal(results[i]['msisdn'], results[i-1]['msisdn'])
+
+    # 拼接结果
+    final_result = pd.concat(results, axis=1)
+    final_result = final_result.loc[:,~final_result.columns.duplicated()]
+
+    return final_result
---
+++ best_archive/f1_66/code/utils/augmentation.py
@@ -0,0 +1,292 @@
+import pandas as pd
+import numpy as np
+from typing import List, Tuple, Callable, Union
+
+
+class Augmentation:
+    """
+    Data augmentation is performed by grouping the data by msisdn, for example, randomly masking a part of the data for msisdn A, and then adding it to the training set
+    """
+
+    def __init__(self, df: pd.DataFrame, label: int, id_column_name: str="msisdn", label_column_name: str = "is_sa"):
+        """
+        Initialize the Augmentation class.
+
+        Args:
+            df (pd.DataFrame): A sequence's all data, with an id column named `id_column`, and without the label column.
+            label (int): The sequence's label.
+            id_column_name (str): The id column name.
+            label_column_name (str): The label column name, which is used to store the label of the new sequences.
+        """
+        self.df = df.drop(columns=[id_column_name])
+        self.label = label
+        self.call_count = 0
+        self.id = df[id_column_name].iloc[0]
+        assert isinstance(self.id, str), f"ID must be a string, but got {type(self.id)}"
+        self.id_column_name = id_column_name
+        self.label_column_name = label_column_name
+
+    def count_calls(func: Callable):
+        def wrapper(self, *args, **kwargs):
+            self.call_count += 1
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    def times(
+        self,
+        times: int,
+        method: str,
+        **method_kwargs,
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """
+        Generate new sequences using the specified augmentation method.
+
+        Args:
+            times (int): The number of new sequences to generate.
+            method (str): The name of the augmentation method to use. Supported methods are ['mask', 'sliding_window', 'max_pooling']
+            method_kwargs (dict): The keyword arguments for the augmentation method. The supported keyword arguments are:
+                'mask': 
+                    ratio (Union[float, List[float], Tuple[float, float]]):
+                        if `ratio` is a list, then randomly choose a ratio from the list.
+                        if `ratio` is a number, then the ratio is the same every time.
+                        if `ratio` is a tuple, then randomly choose a ratio between ratio[0] and ratio[1].
+                'sliding_window':
+                    window_size (int): The size of the sliding window.
+                    step_size (int): The step size for sliding the window.
+                'max_pooling':
+                    window_size (int): The size of the pooling window.
+                    step_size (int): The step size for sliding the window.
+
+        Raises:
+            ValueError: If the specified method is not supported
+    
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: res_dfs, res_labels. res_dfs is all the new sequences, res_labels is the labels of the new sequences: `res_labels[[id_column, label_name]]`, n * 2
+        """
+        method = "_" + method 
+        res_dfs = []
+        method_func = getattr(self, method)
+        assert method_func is not None, f"Method {method} not found"
+        for i in range(times):
+            res_df, label = method_func(**method_kwargs)
+            if res_df is None and label is None:
+                continue
+            else:
+                res_dfs.append(res_df)
+
+        if len(res_dfs) == 0:
+            return None, None
+
+        res_dfs = pd.concat(res_dfs)
+        unique_ids = res_dfs[self.id_column_name].unique()
+        res_labels = pd.DataFrame([self.label] * len(unique_ids), columns=[self.label_column_name])
+        res_labels = pd.concat(
+            [pd.DataFrame(unique_ids, columns=[self.id_column_name]), res_labels], axis=1
+        )
+        return res_dfs, res_labels
+
+    @count_calls
+    def _mask(self, ratio):
+        """
+        Args:
+            ratio (Union[float, List[float], Tuple[float, float]]): if `ratio` is a list, then randomly choose a ratio from the list. if `ratio` is a number, then the ratio is the same every time. if `ratio` is a tuple, then randomly choose a ratio between ratio[0] and ratio[1]
+            
+
+        Raises:
+            ValueError: if ratio is not a valid type of float, list, tuple
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: res_dfs, res_labels. res_dfs is all the new sequences, res_labels is the labels of the new sequences: `res_labels[[id_column, label_name]]`, n * 2
+        """
+        if type(ratio) == list:
+            ratio = np.random.choice(ratio)
+        elif type(ratio) == tuple:
+            ratio = np.random.uniform(ratio[0], ratio[1])
+        elif type(ratio) == int or type(ratio) == float:
+            ratio = ratio
+        else:
+            raise ValueError(f"Invalid ratio type: {type(ratio)}")
+
+        if int(ratio * self.df.shape[0]) < 1:
+            return None, None
+
+        num_rows_to_mask = int(ratio * self.df.shape[0])
+        mask_indices = np.random.choice(
+            self.df.index, size=num_rows_to_mask, replace=False
+        )
+        mask = self.df.index.isin(mask_indices)
+        new_df = self.df[~mask].reset_index(drop=True)
+        new_id = self.id + f"_{self.call_count}"
+        ids = pd.DataFrame([new_id] * new_df.shape[0], columns=[self.id_column_name])
+        res_df = pd.concat([ids, new_df], axis=1, ignore_index=False)
+        return res_df, self.label
+
+    @count_calls
+    def _interpolation(self, label, ratio):
+        """
+        随机插入比例为 ratio 的行
+        """
+        pass
+
+    @count_calls
+    def _noise(self, label, ratio):
+        """
+        为数值类型变量 原值乘以 [1-ratio, 1+ratio] 的随机因子
+        """
+        pass
+
+    @count_calls
+    def _time_smoothing(self, label, ratio):
+        """
+        """
+        pass
+
+    @count_calls
+    def _sliding_window(self, window_size: int, step_size: int):
+        """
+        Create new samples using a sliding window approach.
+
+        Args:
+            window_size (int): The size of the sliding window.
+            step_size (int): The step size for sliding the window.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: res_dfs, res_labels. res_dfs is all the new sequences, res_labels is the labels of the new sequences: `res_labels[[id_column, label_name]]`, n * 2
+        """
+        res_dfs = []
+        total_length = len(self.df)
+
+        # Apply sliding window
+        for start in range(0, total_length - window_size + 1, step_size):
+            end = start + window_size
+            window_df = self.df.iloc[start:end].reset_index(drop=True)
+            if window_df.empty:
+                continue
+            new_id = self.id + f"_{self.call_count}_{start//step_size+1}"
+            ids = pd.DataFrame([new_id] * window_df.shape[0], columns=[self.id_column_name])
+            res_df = pd.concat([ids, window_df], axis=1, ignore_index=False)
+            res_dfs.append(res_df)
+
+        if len(res_dfs) == 0:
+            return None, None
+
+        res_dfs = pd.concat(res_dfs)
+        unique_ids = res_dfs[self.id_column_name].unique()
+        res_labels = pd.DataFrame([self.label] * len(unique_ids), columns=[self.label_column_name])
+        res_labels = pd.concat(
+            [pd.DataFrame(unique_ids, columns=[self.id_column_name]), res_labels], axis=1
+        )
+        return res_dfs, res_labels
+
+    @count_calls
+    def _max_pooling(self, window_size: int, step_size: int):
+        """
+        Apply max pooling to downsample the time series data by selecting the most frequent value in each column.
+
+        Args:
+            window_size (int): The size of the pooling window.
+            step_size (int): The step size for sliding the window.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: res_dfs, res_labels. res_dfs is all the new sequences, res_labels is the labels of the new sequences: `res_labels[[id_column, label_name]]`, n * 2
+        """
+        res_dfs = []
+        total_length = len(self.df)
+
+        for start in range(0, total_length - window_size + 1, step_size):
+            end = start + window_size
+            window_df = self.df.iloc[start:end]
+            if window_df.empty:
+                continue
+
+            # Find the most frequent value in each column
+            pooled_df = window_df.mode().iloc[0].to_frame().T
+            new_id = f"{self.id}_{self.call_count}"
+            ids = pd.DataFrame([new_id] * pooled_df.shape[0], columns=[self.id_column_name])
+            res_df = pd.concat([ids, pooled_df], axis=1, ignore_index=False)
+            res_dfs.append(res_df)
+
+        if len(res_dfs) == 0:
+            return None, None
+
+        res_dfs = pd.concat(res_dfs)
+        unique_ids = res_dfs[self.id_column_name].unique()
+        res_labels = pd.DataFrame([self.label] * len(unique_ids), columns=[self.label_column_name])
+        res_labels = pd.concat(
+            [pd.DataFrame(unique_ids, columns=[self.id_column_name]), res_labels], axis=1
+        )
+        return res_dfs, res_labels
+
+# 下面是一个简单的使用例子
+if __name__ == "__main__":
+    from tqdm import tqdm
+
+    train_data = pd.read_csv(
+        "../data/raw/trainSet_res_with_distances.csv", dtype={"msisdn": "str"}
+    )
+    train_labels = pd.read_csv("../data/raw/trainSet_ans.csv", dtype={"msisdn": "str"})
+
+    validation_data = pd.read_csv(
+        "../data/raw/validationSet_res_with_distances.csv", dtype={"msisdn": "str"}
+    )
+
+    train_data["start_time"] = pd.to_datetime(
+        train_data["start_time"], format="%Y%m%d%H%M%S"
+    )
+    train_data["end_time"] = pd.to_datetime(
+        train_data["end_time"], format="%Y%m%d%H%M%S"
+    )
+    train_data["open_datetime"] = pd.to_datetime(
+        train_data["open_datetime"], format="%Y%m%d%H%M%S"
+    )
+    train_data["update_time"] = pd.to_datetime(train_data["update_time"])
+    train_data["date"] = pd.to_datetime(train_data["date"])
+
+    validation_data["start_time"] = pd.to_datetime(
+        validation_data["start_time"], format="%Y%m%d%H%M%S"
+    )
+    validation_data["end_time"] = pd.to_datetime(
+        validation_data["end_time"], format="%Y%m%d%H%M%S"
+    )
+    validation_data["open_datetime"] = pd.to_datetime(
+        validation_data["open_datetime"], format="%Y%m%d%H%M%S", errors="coerce"
+    )
+    validation_data["update_time"] = pd.to_datetime(validation_data["update_time"])
+    validation_data["date"] = pd.to_datetime(validation_data["date"])
+
+    addition_train_data = []
+    addition_train_labels = []
+
+    times = 1
+    ratio_range = 0.1
+    window_size = 10  # 设置滑动窗口的大小
+    step_size = 5  # 设置滑动窗口的步长
+    pbar = tqdm(train_data.groupby("msisdn"))
+    for msisdn, group in pbar:
+        if msisdn == 0:
+            continue
+        pbar.set_description(f"Augmenting msisdn {msisdn}")
+        label = train_labels[train_labels["msisdn"] == msisdn].iloc[0]["is_sa"]
+        aug = Augmentation(group, label, "msisdn", "is_sa")
+        # 对正负样本进行平衡 样本比 1:4
+        if label == 1:
+            res_df, res_labels = aug.times(
+                times=times * 4,
+                window_size=window_size, step_size=step_size,
+                method="max_pooling"
+            )
+        else:
+            res_df, res_labels = aug.times(
+                times=times,
+                window_size=window_size, step_size=step_size,
+                method="max_pooling"
+            )
+        if res_df is not None and res_labels is not None:
+            addition_train_data.append(res_df)
+            addition_train_labels.append(res_labels)
+
+    addition_train_data = pd.concat(addition_train_data)
+    addition_train_labels = pd.concat(addition_train_labels)
+    print("addition_train_data.shape is: ", addition_train_data.shape)
+    addition_train_data.to_csv("addition_train_data.csv", index=False)  
