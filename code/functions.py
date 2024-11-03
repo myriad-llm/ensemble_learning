d79d3ee3d3e2b30e2ad1b2abb2b3822a8afda233
ShiWenber <1210169842@qq.com>
2024-11-03 14:49:10 +0800

    perf: Agg in parallel

--- code/functions.py
+++ code/functions.py
@@ -1,9 +1,9 @@
+import os
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import pandas as pd
 from tqdm import tqdm
 import time
 from utils.augmentation import Augmentation
-import multiprocessing
 import warnings
 
 def augment_data(train_data, train_labels, test_labels):
@@ -82,7 +82,7 @@ def _augment_group(group_data, train_labels):
 
 def augment_data_parallel(train_data, train_labels, test_labels, max_workers=0):
     if max_workers == 0:
-        max_workers = multiprocessing.cpu_count()
+        max_workers = os.cpu_count()
 
     # Split the data into max_workers groups
     groups = [group for _, group in train_data.groupby('msisdn')]
@@ -125,8 +125,20 @@ def augment_data_parallel(train_data, train_labels, test_labels, max_workers=0):
 
     return train_data, train_labels, labels_aug
 
+
+def get_nan(train):
+    # 获取 train 中的 nan值
+    train_nan = train[train.isnull().T.any()]
+    # 统计 每列含有的 nan 数量
+    for col in train.columns:
+        if train[col].isnull().sum() > 0:
+            print(col, train[col].isnull().sum())
+
+    return train_nan
+
 # 聚合特征
 def aggregate_features(data):
+    warnings.warn("This function will be deprecated in the future, use aggregate_features_parallel instead.", DeprecationWarning)
     agg_features = data.groupby('msisdn').agg({
     'call_duration': [
         ('sum', 'sum'), 
@@ -213,12 +225,211 @@ def aggregate_features(data):
     agg_features = agg_features.reset_index()
     return agg_features
 
-def get_nan(train):
-    # 获取 train 中的 nan值
-    train_nan = train[train.isnull().T.any()]
-    # 统计 每列含有的 nan 数量
-    for col in train.columns:
-        if train[col].isnull().sum() > 0:
-            print(col, train[col].isnull().sum())
+class _CustomAggregator:
+    @staticmethod
+    def quantile_25(x):
+        return x.quantile(0.25)
 
-    return train_nan
+    @staticmethod
+    def quantile_50(x):
+        return x.quantile(0.5)
+
+    @staticmethod
+    def quantile_75(x):
+        return x.quantile(0.75)
+
+    # just for column 'dayofweek'
+    @staticmethod
+    def magic(x):
+        return x.value_counts().mean()
+
+    # just for column 'dayofweek'
+    @staticmethod
+    def work_day_num(x):
+        return x[x.isin([1, 2, 3, 4, 5])].count()
+
+    @staticmethod
+    def weekend_num(x):
+        return x[x.isin([6, 7])].count()
+
+    @staticmethod
+    def mode(x):
+        return x.mode().values[0]
+
+    @staticmethod
+    def work_day_weekend_diff(x):
+        work_days = x[x.isin([1, 2, 3, 4, 5])].count()
+        weekends = x[x.isin([6, 7])].count()
+        return (work_days - weekends) / (work_days + weekends)
+
+    @staticmethod
+    def times_not_at_home_area(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def called_diff_home_code(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def diff(x):
+        return x[x != x.shift()].count() / x.count()
+
+    @staticmethod
+    def call_num(x):
+        return x[x.isin([1, 3])].count()
+
+    @staticmethod
+    def called_num(x):
+        return x[x == 2].count()
+
+    @staticmethod
+    def call_called_normalized_diff(x):
+        calls = x[x.isin([1, 3])].count()
+        called = x[x == 2].count()
+        return (calls - called) / (calls + called)
+
+    @staticmethod
+    def coefficient_of_variation(x):
+        return x.std() / x.mean()
+
+def _aggregate_features(data, agg_params):
+    agg_features = data.groupby('msisdn').agg(agg_params)
+    agg_features.columns = ['+'.join(col).strip() for col in agg_features.columns.values]
+    agg_features.columns = agg_features.columns.str.replace('[', '').str.replace(']', '').str.replace('<', '').str.replace('>', '').str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace(' ', '_')
+    agg_features = agg_features.reset_index()
+    return agg_features
+
+# 并行处理函数
+def aggregate_features_parallel(data, max_workers=0):
+    agg_params = {
+        'call_duration': [
+            ('sum', 'sum'), 
+            ('mean', 'mean'), 
+            ('max', 'max'), 
+            ('std', 'std'),
+            ('quantile_25', _CustomAggregator.quantile_25),
+            ('quantile_50', _CustomAggregator.quantile_50),
+            ('quantile_75', _CustomAggregator.quantile_75),
+        ],
+        'cfee': [
+            ('sum', 'sum'),
+            ('std', 'std'), 
+            ('mean', 'mean'),
+        ],
+        'lfee': [
+            ('sum', 'sum'), 
+            ('mean', 'mean'),
+            ('std', 'std'),
+        ],
+        'hour': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'dayofweek': [
+            ('std', 'std'), 
+            ('magic', _CustomAggregator.magic),
+            ('work_day_num', _CustomAggregator.work_day_num),
+            ('weekend_num', _CustomAggregator.weekend_num),
+            ('mode', _CustomAggregator.mode),
+            ('work_day_weekend_diff', _CustomAggregator.work_day_weekend_diff),
+        ],
+        'visit_area_code': [
+            ('nunique', 'nunique'),
+            ('times_not_at_home_area', _CustomAggregator.times_not_at_home_area)
+        ],
+        'called_home_code': [
+            ('nunique', 'nunique'),
+            ('called_diff_home_code', _CustomAggregator.called_diff_home_code)
+        ],
+        'called_code': [
+            ('nunique', 'nunique'),
+            ('diff', _CustomAggregator.diff)
+        ],
+        'open_datetime': [
+            ('open_count', 'nunique')
+        ],
+        'other_party': [
+            ('account_person_num', 'nunique'),
+            ('called_diff_home_code', _CustomAggregator.called_diff_home_code)
+        ],
+        'a_serv_type': [
+            ('call_num', _CustomAggregator.call_num), 
+            ('called_num', _CustomAggregator.called_num),
+            ('call_called_normalized_diff', _CustomAggregator.call_called_normalized_diff),
+        ],
+        'start_time_diff': [
+            ('start_time_diff_mean', 'mean'), 
+            ('start_time_diff_std', 'std'), 
+            ('max', 'max'), 
+            ('coefficient_of_variation', _CustomAggregator.coefficient_of_variation),
+        ], 
+        'phone1_loc_city_lat': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone1_loc_city_lon': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone2_loc_city_lat': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'phone2_loc_city_lon': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'start_day': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+        'start_day_diff': [
+            ('mean', 'mean'), 
+            ('std', 'std'), 
+            ('max', 'max'), 
+            ('min', 'min'),
+        ],
+    }
+
+    if max_workers == 0:
+        max_workers = min(os.cpu_count(), len(agg_params))
+    print(f'Using {max_workers} workers to aggregate features...')
+
+    columns = [col for col in data.columns if col in agg_params.keys()]
+    results = []
+
+    unused_params = [i for i in agg_params.keys() if i not in columns]
+    if unused_params:
+        print(f"The following aggregate params are not found in the dataframe and will be ignored:\n{', '.join(unused_params)}")
+
+    with ProcessPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(_aggregate_features, data[['msisdn', col]], agg_params[col]): col for col in columns}
+        for future in as_completed(futures):
+            col = futures[future]
+            try:
+                result = future.result()
+                results.append(result)
+            except Exception as exc:
+                print(f'{col} generated an exception: {exc}')
+
+    # # 验证每个结果的 msisdn 是否一致
+    # for i in range(1, len(results)):
+    #     pd.testing.assert_series_equal(results[i]['msisdn'], results[i-1]['msisdn'])
+
+    # 拼接结果
+    final_result = pd.concat(results, axis=1)
+    final_result = final_result.loc[:,~final_result.columns.duplicated()]
+
+    return final_result
